fa8c29cc823d8d7956e1303ff2ade722ea0afdab
arthur <arthurmatheusfreitas@gmail.com>
2024-10-16 08:35:08 -0300

    ghdfgd

--- Passarinho/MainPage.xaml.cs
+++ Passarinho/MainPage.xaml.cs
@@ -8,45 +8,116 @@ public partial class MainPage : ContentPage
 	const int Gravidade = 1;
 	const int TempoEntreFrames = 25;//ms
 	bool EstaMorto = false;
+	const int ForcaPulo = 30;
+	const int MaxTempoPulando = 3;
+	bool EstaPulando = false;
+	int tempoPulando = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-    void AplicaGravidade()
+	void AplicaGravidade()
 	{
-		urubu.TranslationY+=Gravidade; 
+		urubu.TranslationY += Gravidade;
 	}
-	
+
 	async Task Desenha()
 	{
 		while (!EstaMorto)
 		{
 			GerenciaCanos();
 			AplicaGravidade();
+			if (VericaColizao())
+			{
+				EstaMorto = true;
+				frameGameOver.IsVisible = true;
+				break;
+			}
 			await Task.Delay(TempoEntreFrames);
+
+		}
+		if (EstaPulando)
+			AplicaPulo();
+		else
+			AplicaGravidade();
+		void AplicaPulo()
+		{
+			urubu.TranslationY -= ForcaPulo;
+			tempoPulando++;
+			
+			if (tempoPulando >= MaxTempoPulando)
+			{
+				EstaPulando = false;
+				tempoPulando = 0;
+			}
+		}
+
+	}
+
+	void OnGridClicked(object s, TappedEventArgs a)
+	{
+		EstaPulando = true;
+	}
+
+	void OnGameOverClicked(object s, TappedEventArgs e)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenha();
+	}
+
+	void Inicializar()
+	{
+		urubu.TranslationY = 0;
+	}
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		LarguraJanela = w;
+		AlturaJanela = h;
+	}
+	void GerenciaCanos()
+	{
+		imgcactocima.TranslationX -= Velocidade;
+		imgcactobaixo.TranslationX -= Velocidade;
+		if (imgcactobaixo.TranslationX < -LarguraJanela)
+		{
+			imgcactobaixo.TranslationX = 0;
+			imgcactocima.TranslationX = 0;
 		}
 	}
-	
-			void Inicializar()
+	bool VerificaColizaoTeto()
+	{
+		var minY = -AlturaJanela / 2;
+
+		if (urubu.TranslationY <= minY)
+			return true;
+		else
+			return false;
+	}
+
+	bool VerificaColizaoChao()
+	{
+		var maxY = AlturaJanela / 2 - imgChao.HeightRequest - 210;
+
+		if (urubu.TranslationY >= maxY)
+			return true;
+		else
+			return false;
+	}
+
+	bool VericaColizao()
+	{
+		if (!EstaMorto)
+		{
+			if (VerificaColizaoTeto() || VerificaColizaoChao())
 			{
-				urubu.TranslationY=0;
+				return true;
 			}
-			protected override void OnSizeAllocated(double w, double h)
-			{
-				base.OnSizeAllocated(w, h);
-				LarguraJanela = w;
-				AlturaJanela = h;
-			}
-			void GerenciaCanos()
-			{
-				imgcactocima.TranslationY-= Velocidade;
-				imgcactobaixo.TranslationX-= Velocidade;
-				if(imgcactobaixo.TranslationX<-LarguraJanela)
-				{
-					imgcactobaixo.TranslationX = 0;
-					imgcactocima.TranslationX = 0;
-				}
-			}
+		}
+
+		return false;
+	}
 }
 
